5fd3459d9633503b3673f2c6d1f67f7b88dc8ef0
Vivek Sekhar <vivek@viveksekhar.ca>
2011-09-03 09:52:18 -0400

    Disambiguate 'launch' helper module as 'local'

--- launch.py
+++ launch.py
@@ -8,7 +8,7 @@ import subprocess
 
 import boto
 
-import launch
+import local
 import setup
 
 def launch_local(config):
@@ -17,7 +17,7 @@ def launch_local(config):
 		configfile.flush()
 		configfile.seek(0)
 		command = 'twistd -n vmesh --local'
-		if launch.get_arg('debug'):
+		if local.get_arg('debug'):
 			command += ' --debug'
 		command += ' --config-file ' + configfile.name
 		print 'Launching with command: %s' % command
@@ -33,59 +33,59 @@ def launch_remote(user_data):
 	import StringIO, gzip
 	from contextlib import closing
 
-	if launch.get_arg('spot_instances'):
-		requests = launch.ec2.request_spot_instances(
-								price=launch.get_arg('price'),
-								image_id=launch.get_arg('ami'),
-								count=launch.get_arg('count'),
-								instance_type=launch.get_arg('instance_type'),
-								type=('persistent' if launch.get_arg('persistent') else 'one-time'),
-								key_name=launch.get_arg('key_pair'),
-								security_groups=launch.get_arg('security_groups'),
+	if local.get_arg('spot_instances'):
+		requests = local.ec2.request_spot_instances(
+								price=local.get_arg('price'),
+								image_id=local.get_arg('ami'),
+								count=local.get_arg('count'),
+								instance_type=local.get_arg('instance_type'),
+								type=('persistent' if local.get_arg('persistent') else 'one-time'),
+								key_name=local.get_arg('key_pair'),
+								security_groups=local.get_arg('security_groups'),
 								user_data=user_data
 								)
 		return requests
 	else:
-		reservation = launch.ec2.run_instances(
-								image_id=launch.get_arg('ami'),
-								min_count=launch.get_arg('count'),
-								max_count=launch.get_arg('count'),
-								key_name=launch.get_arg('key_pair'),
-								security_groups=launch.get_arg('security_groups'),
-								instance_type=launch.get_arg('instance_type'),
+		reservation = local.ec2.run_instances(
+								image_id=local.get_arg('ami'),
+								min_count=local.get_arg('count'),
+								max_count=local.get_arg('count'),
+								key_name=local.get_arg('key_pair'),
+								security_groups=local.get_arg('security_groups'),
+								instance_type=local.get_arg('instance_type'),
 								user_data=user_data
 								)
 		return reservation
 
 def launch_bare():
-	return launch.ec2.run_instances(
-				image_id=launch.get_arg('ami'),
-				min_count=launch.get_arg('count'),
-				max_count=launch.get_arg('count'),
-				key_name=launch.get_arg('key_pair'),
-				security_groups=launch.get_arg('security_groups'),
-				instance_type=launch.get_arg('instance_type')
+	return local.ec2.run_instances(
+				image_id=local.get_arg('ami'),
+				min_count=local.get_arg('count'),
+				max_count=local.get_arg('count'),
+				key_name=local.get_arg('key_pair'),
+				security_groups=local.get_arg('security_groups'),
+				instance_type=local.get_arg('instance_type')
 				)
 
 
 if __name__ == '__main__':
-	if launch.get_arg('bare'):
+	if local.get_arg('bare'):
 		print launch_bare()
 		sys.exit(0)
 
-	bucketname = launch.get_arg('bucket')
-	# packagename = launch.get_arg('package_name')
-	eggfilename = launch.get_arg('egg_file_name')
+	bucketname = local.get_arg('bucket')
+	# packagename = local.get_arg('package_name')
+	eggfilename = local.get_arg('egg_file_name')
 
 	# slipstream credentials into config file
-	from launch.include import load_with_includes
+	from local.include import load_with_includes
 	passthrough = ['node_access_key', 'node_secret_key', 'bucket', 'egg_file_name', 'package_name']
 	config = load_with_includes('vmesh-config.txt', passthrough=passthrough)
-	if launch.get_arg('config_only'):
+	if local.get_arg('config_only'):
 		print config
 		sys.exit(0)
 
-	if launch.get_arg('local'):
+	if local.get_arg('local'):
 		ret = launch_local(config)
 		sys.exit(ret)
 
@@ -97,7 +97,7 @@ if __name__ == '__main__':
 	egg_path += '-py%d.%d.egg' % (sys.version_info[0], sys.version_info[1])
 
 	# upload the egg
-	b = launch.s3.get_bucket(bucketname)
+	b = local.s3.get_bucket(bucketname)
 	if not b:
 		b = s3.create_bucket(bucketname)
 	k = b.get_key(eggfilename)
@@ -111,12 +111,13 @@ if __name__ == '__main__':
 	k.set_contents_from_file(open(egg_path, mode='rb'), cb=report_progress)
 	print ' done'
 
-	if launch.get_arg('upload_only'):
+	if local.get_arg('upload_only'):
 		sys.exit(0)
 
 	# create multipart userdata
 	with tempfile.NamedTemporaryFile() as configfile:
 		configfile.write(config)
+		configfile.flush()
 		configfile.seek(0)
 		command = 'write-mime-multipart -z'
 		command += ' cloud-config'
